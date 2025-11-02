0362b931a62dd74d54e9e6b94784dad18d31bfd4
PavelMartinelli <pav.shipitsyn@gmail.com>
2025-11-02 13:02:12 +0500

    Create project

---
+++ cs/Markdown/Md.cs
@@ -0,0 +1,10 @@
+﻿namespace Markdown;
+
+public class Md
+{
+    public string Render(string markdownText)
+    {
+        // Основной метод для преобразования Markdown в HTML
+        throw new NotImplementedException();
+    }
+}
