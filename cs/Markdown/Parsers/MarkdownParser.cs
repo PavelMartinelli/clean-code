f106dc9f7814429694522a6958014d4abcec2869
PavelMartinelli <pav.shipitsyn@gmail.com>
2025-11-02 18:50:57 +0500

    Add ParserContext and MarkdownParser without implementation

---
+++ cs/Markdown/Parsers/MarkdownParser.cs
@@ -0,0 +1,18 @@
+﻿using Markdown.Interfaces;
+
+namespace Markdown.Parsers;
+
+public class MarkdownParser : IParser
+{
+    private readonly List<ITokenHandler> handlers;
+    
+    public MarkdownParser(IEnumerable<ITokenHandler> handlers)
+    {
+        this.handlers = handlers.ToList();
+    }
+
+    public IEnumerable<Token> Parse(string markdown)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ cs/Markdown/Parsers/ParserContext.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+namespace Markdown.Parsers;
+
+public class ParserContext
+{
+    public string SourceText { get; set; }
+    public int Position { get; set; }
+    public StringBuilder CurrentText { get; set; }
+    public Stack<Token> TokenStack { get; set; }
+    public List<Token> ResultTokens { get; set; }
+    public bool IsEscaped { get; set; }
+    
+    public ParserContext()
+    {
+        CurrentText = new StringBuilder();
+        TokenStack = new Stack<Token>();
+        ResultTokens = new List<Token>();
+    }
+    
+    public void FlushCurrentText()
+    {
+        if (CurrentText.Length <= 0) 
+            return;
+        ResultTokens.Add(new Token(TokenType.Text, CurrentText.ToString()));
+        CurrentText.Clear();
+    }
+}
